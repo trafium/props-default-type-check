a6ecae6ab1ab06aeae23de1eb04955abdc406b04
traf <trafium@gmail.com>
2021-12-04 17:50:26 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+
+  type User = {
+    name : string;
+  }
+
+  export default defineComponent({
+    props: {
+      user: {
+        type: Object as PropType<User>,
+        default: () => '123',
+      },
+    },
+
+    setup (props) {
+      console.log(props.user);
+    },
+  });
+</script>
+
+<template>
+  <div>Hello, world</div>
+</template>
